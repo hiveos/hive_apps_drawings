e4ddad5a407c8fce3b22a7bf333acef5aa9dc2ef
Ihsan Isik <x.ihsan@gmail.com>
2013-12-31 22:48:07 +0100

    Create .nomedia file at startup in order to hide exported drawings from gallery app

--- src/com/example/drawing/Browser.java
+++ src/com/example/drawing/Browser.java
@@ -1,22 +1,15 @@
 package com.example.drawing;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import android.annotation.SuppressLint;
-import android.app.ActionBar;
-import android.app.ActionBar.Tab;
-import android.app.AlertDialog;
 import android.app.ListActivity;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
@@ -25,13 +18,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 @SuppressLint("NewApi")
 public class Browser extends ListActivity {
 
 	private List<String> items = null;
-	private File Lokacija = new File(
-			Environment.getExternalStorageDirectory() + "/HIVE/Drawings/");
+	private File Lokacija = new File(Environment.getExternalStorageDirectory()
+			+ "/HIVE/Drawings/");
 	public static Bitmap LoadaniCrtez;
 
 	@Override
@@ -40,11 +34,23 @@ public class Browser extends ListActivity {
 		setContentView(R.layout.activity_browser);
 		if (!Lokacija.exists())
 			Lokacija.mkdirs();
-		getFiles(new File(Environment.getExternalStorageDirectory() + "/HIVE/Drawings/")
-				.listFiles());
+		getFiles(new File(Environment.getExternalStorageDirectory()
+				+ "/HIVE/Drawings/").listFiles());
+
+		File nomedia = new File(Environment.getExternalStorageDirectory()
+				+ "/HIVE/Drawings/.nomedia");
+		try {
+			nomedia.createNewFile();
+
+			FileOutputStream ostream;
+			ostream = new FileOutputStream(nomedia);
+			ostream.flush();
+			ostream.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
-	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -56,9 +62,11 @@ public class Browser extends ListActivity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// TODO Auto-generated method stub
-		switch(item.getItemId()){
+		switch (item.getItemId()) {
 		case R.id.action_add:
-			File initialDrawing = new File(Environment.getExternalStorageDirectory() + "/HIVE/Drawings/Drawing0.png");
+			File initialDrawing = new File(
+					Environment.getExternalStorageDirectory()
+							+ "/HIVE/Drawings/Drawing0.png");
 			Bitmap.Config conf = Bitmap.Config.ARGB_8888;
 			LoadaniCrtez = Bitmap.createBitmap(10, 10, conf);
 			LoadaniCrtez.recycle();
@@ -71,25 +79,22 @@ public class Browser extends ListActivity {
 		}
 	}
 
-
-
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		int selectedRow = (int) id;
 
-		
-			File file = new File(items.get(selectedRow));
-			String imeFajla = file.getName().toString();
-			String ekstenzijaFajla = imeFajla.substring(
-					(imeFajla.lastIndexOf(".") + 1), imeFajla.length());
-			String NameWithoutExtension = imeFajla.split("\\.")[0];
+		File file = new File(items.get(selectedRow));
+		String imeFajla = file.getName().toString();
+		String ekstenzijaFajla = imeFajla.substring(
+				(imeFajla.lastIndexOf(".") + 1), imeFajla.length());
+		String NameWithoutExtension = imeFajla.split("\\.")[0];
 
-			if (file.isFile() && ekstenzijaFajla.equals("png")) {
-					//AKO JE FAJL PNG
-				LoadaniCrtez = BitmapFactory.decodeFile(file.getAbsolutePath());
-				Intent myIntent = new Intent(Browser.this, MainActivity.class);
-				myIntent.putExtra("Drawing Name", NameWithoutExtension);
-				Browser.this.startActivity(myIntent);
+		if (file.isFile() && ekstenzijaFajla.equals("png")) {
+			// AKO JE FAJL PNG
+			LoadaniCrtez = BitmapFactory.decodeFile(file.getAbsolutePath());
+			Intent myIntent = new Intent(Browser.this, MainActivity.class);
+			myIntent.putExtra("Drawing Name", NameWithoutExtension);
+			Browser.this.startActivity(myIntent);
 		}
 	}
 
